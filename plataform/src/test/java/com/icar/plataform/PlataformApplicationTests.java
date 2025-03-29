2a285f61366435f55103fd3eeaeb3377e318b682
Felipe Ferreira <100241185+felipeov1@users.noreply.github.com>
2025-03-29 11:06:38 -0300

    Restructuring repository to include frontend and backend

---
+++ plataform/src/test/java/com/icar/plataform/PlataformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icar.plataform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
