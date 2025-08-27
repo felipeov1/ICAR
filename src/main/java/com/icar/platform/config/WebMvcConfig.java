03b27b221e4f0a771507018313fb47271e221c38
Felipe Ferreira <ofelipe439@gmail.com>
2025-08-27 20:49:38 -0300

    Fix: Corrected WebMvcConfig to use absolute path for static resources

--- src/main/java/com/icar/platform/config/WebMvcConfig.java
+++ src/main/java/com/icar/platform/config/WebMvcConfig.java
@@ -1,26 +1,20 @@
 package com.icar.platform.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.nio.file.Paths;
-
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    @Value("${storage.location}")
-    private String storageLocation;
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        String uploadPathUri = Paths.get(storageLocation).toAbsolutePath().toUri().toString();
+        String absoluteUploadsPath = "file:///var/www/icarplus/uploads/";
 
         registry.addResourceHandler("/uploads/static/**")
-                .addResourceLocations(uploadPathUri + "static/");
+                .addResourceLocations(absoluteUploadsPath + "static/");
 
         registry.addResourceHandler("/uploads/carwash/**")
-                .addResourceLocations(uploadPathUri + "carwash/");
+                .addResourceLocations(absoluteUploadsPath + "carwash/");
     }
 }
