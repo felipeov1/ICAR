7da89f9747c5ff443de7fa11e9bcac11ec29ad4c
Felipe Ferreira <ofelipe439@gmail.com>
2025-08-27 19:07:58 -0300

    new deploy

--- src/main/java/com/icar/platform/config/WebMvcConfig.java
+++ src/main/java/com/icar/platform/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.icar.platform.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,10 +10,13 @@ import java.nio.file.Paths;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    @Value("${storage.location}")
+    private String storageLocation;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        String uploadPathUri = Paths.get("uploads").toAbsolutePath().toUri().toString();
-
+        // Converte o caminho do storageLocation em um URI
+        String uploadPathUri = Paths.get(storageLocation).toAbsolutePath().toUri().toString();
 
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations(uploadPathUri);
