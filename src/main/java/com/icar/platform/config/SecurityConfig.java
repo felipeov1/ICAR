e0c5435c4d0687448e21c8a9305c579b17311615
Felipe Ferreira <ofelipe439@gmail.com>
2025-08-31 18:51:15 -0300

    test cors

--- src/main/java/com/icar/platform/config/SecurityConfig.java
+++ src/main/java/com/icar/platform/config/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(Customizer.withDefaults())
+//                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
