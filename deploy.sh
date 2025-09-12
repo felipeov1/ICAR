79e55f140ae0fac8fa625f429ade4887f57c0551
Felipe Oliveira <ofelipe439@gmail.com>
2025-09-12 08:51:28 +0000

    Fix: Remove a diretiva de log do comando de start no script de deploy

--- deploy.sh
+++ deploy.sh
@@ -82,6 +82,7 @@ nohup java \
     -Dstorage.base-url="https://api.icarplus.com.br" \
     -Dstorage.location="/var/www/icarplus/uploads" \
     -jar /opt/icarplus/backend/target/plataform-0.0.1-SNAPSHOT.jar \
+    #--logging.level.root=INFO > "$APP_LOG_FILE" 2>&1 & 
 
 sleep 15
 NEW_PID=$(lsof -t -i:$APP_PORT || echo "")
