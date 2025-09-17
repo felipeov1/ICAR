fb958af90591ef74cccb2616e4ad6cec7c32b33d
Felipe Ferreira <ofelipe439@gmail.com>
2025-09-16 21:23:52 -0300

    mig alt

--- target/classes/db/migration/V81__warning_subscription.sql
+++ target/classes/db/migration/V81__warning_subscription.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS dry_wash_subscriptions (
     id UUID PRIMARY KEY NOT NULL,
     customer_id UUID NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_subscription_customer FOREIGN KEY(customer_id) REFERENCES customers(id),
+    CONSTRAINT fk_subscription_customer FOREIGN KEY(customer_id) REFERENCES customer(id),
     CONSTRAINT uk_customer_id UNIQUE (customer_id)
 );
